1b09556c31985bd3388bd36a4d51610fe24ff821
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-04-27 16:32:55 +0200

    [UNTESTED] Now reports directly to aw-server

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ var { setInterval, clearInterval } = require("sdk/timers");
 
 var { ToggleButton } = require("sdk/ui/button/toggle");
 var panels = require("sdk/panel");
+var Request = require("sdk/request").Request;
 
 var settings = {
     log_inactive: true,
-    loggingInterval: 5000
+    loggingInterval: 5000,
+    server_enabled: true,
+    server_hostname: "localhost",
+    server_port: "5000"
 }
 
 var state = {
@@ -43,14 +47,41 @@ function handleChange(state) {
       panel.show({position: button});
 }
 
-
 function save_tabs(tabs) {
     if(ss.storage.tab_history === undefined) {
         ss.storage.tab_history = {};
     }
 
-    let nowstr = "2016-04-02 23:18:15";
-    ss.storage.tab_history[nowstr] = tabs;
+    let now = new Date();
+
+    // Will fill up quick, so don't do without ensuring it doesn't fill up
+    // TODO: Do something smart
+    //ss.storage.tab_history[now.toISOString()] = tabs;
+
+    // Send to actwa-server
+    var event = {
+        "tags": "firefox-watcher",
+        "tabs": tabs,
+        "timestamp": now.toISOString()
+    }
+
+    let url = "http://" + settings["server_hostname"] + ":" + settings["server_port"] + "/api/0/activity/firefoxwatcher";
+    let headers = {
+        "Content-Type": "application/json",
+        // TODO: Use authorization in the future
+        //"Authorization": "Basic " + %Base64(username + ":" + "password")
+    }
+
+    r = Request({
+        "url": url,
+        "headers": headers,
+        "content": events,
+        "contentType": "application/json",
+        "onComplete": function(response) {
+            console.log("Completed request to actwa-server");
+        }
+    })
+    r.post();
 }
 
 
@@ -89,7 +120,8 @@ function startLogging() {
 
     // Run once immediately
     log();
-    // Then log every state.loggingInterval seconds
+
+    // Then log every state.loggingInterval milliseconds
     state.intervalId = setInterval(log, settings.loggingInterval || 5000);
 }
 
